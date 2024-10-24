4e8b1e22dcb7fad29868b94d2ffb945eaecf45fe
Moisés <olimpiodev.tec@gmail.com>
2024-10-24 14:30:13 -0300

    Atualizando projeto
    Projeto funcionando v1

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+
+load_dotenv()
 
 app = Flask(__name__)
+CONTEXT = 'Responda como um esportista'
+error_message = ''
+
+try:
+    model = genai.GenerativeModel('gemini-1.0-pro-latest')
+    genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
+except Exception as error:
+    error_message = f'Erro ao carregar o modelo: {error}'
 
 
 @app.route('/')
 def index():
-    return 'ok'
+    if len(error_message) == 0:
+        return render_template('index.html')
+    else:
+        return {'message': error_message}
+
+
+@app.route('/search')
+def search():
+    prompt = request.args.get('prompt')
+    try:
+        input_ia = f'{CONTEXT}: {prompt}'
+        output = model.generate_content(input_ia)
+        return {'message': output.text}
+    except Exception as ex:
+        print(f'Erro ao gerar resposta: {ex}')
+        return 'Ocorreu um erro ao processar sua solicitação :)'
 
 
 if __name__ == '__main__':
